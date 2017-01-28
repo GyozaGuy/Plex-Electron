affa0cc0a448fe144c5c27c19628956ed059f0d2
GyozaGuy <gyozaguy@gmail.com>
2017-01-27 23:31:00 -0700

    Updated build scripts, ignored releases folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 APPNAME=Plex-Electron
 VERSION=1.5.0
-electron-packager . $APPNAME --platform=linux --arch=x64 --version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
+electron-packager . $APPNAME --platform=linux --arch=x64 --electron-version=$VERSION --overwrite=true --asar=false --app_version=0.0.1 --appname=$APPNAME --out=releases --overwrite=true --icon=images/app.png
